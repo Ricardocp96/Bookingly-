cf9dc78ce88cc82068a7e8c90bcda24893768345
Richard Lubanga <ricardo@Richards-MacBook-Pro.local>
2022-10-09 15:07:17 +0300

    added new host

--- hms/hms/settings.py
+++ hms/hms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(84j*^r#6u2p(sq706v%!3_1nn(&ad@10r7y1e9k^o7l$z9rp=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.197.36.210']
 
 
 # Application definition
